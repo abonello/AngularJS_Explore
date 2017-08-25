89c85fa206894b0c02303aa99b71a942d55f8593
abonello <ab@anthonybonello.co.uk>
2017-08-26 00:27:56 +0100

    Two-way binding

--- js/binding.js
+++ js/binding.js
@@ -2,7 +2,7 @@ var app = angular.module("app", []);
 app.controller("oneWay", ["$scope", function($scope) {
     $scope.a = 10;
     $scope.b = 20;
-}]);
+}])
 app.controller("oneTime", ["$scope", function($scope) {
     $scope.c = 10;
     $scope.d = 20;
@@ -15,4 +15,12 @@ app.controller("oneTime", ["$scope", function($scope) {
     $scope.sumValues = function() {
         $scope.sum = $scope.c + $scope.d;
     };
-}]);
+}])
+app.controller("twoWay", ["$scope", function($scope) {
+    $scope.e = 10;
+    $scope.f = 15;
+
+    $scope.addNumbers = function() {
+        $scope.sum = parseInt($scope.e) + parseInt($scope.f); //The parsing is required as we will be getting input from user as a string.
+    };
+}])
