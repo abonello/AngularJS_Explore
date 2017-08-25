a9938f5531eadbe43073fb5274406635de3e6fd6
abonello <ab@anthonybonello.co.uk>
2017-08-25 21:42:06 +0100

    Finished One-time only section

--- js/binding.js
+++ js/binding.js
@@ -3,3 +3,16 @@ app.controller("oneWay", ["$scope", function($scope) {
     $scope.a = 10;
     $scope.b = 20;
 }]);
+app.controller("oneTime", ["$scope", function($scope) {
+    $scope.c = 10;
+    $scope.d = 20;
+
+    $scope.doubleValues = function() {
+        $scope.c = $scope.c * 2;
+        $scope.d = $scope.d * 2;
+    };
+
+    $scope.sumValues = function() {
+        $scope.sum = $scope.c + $scope.d;
+    };
+}]);
